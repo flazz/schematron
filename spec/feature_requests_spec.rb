d24f8056e7d1bbf251671c07b3c54a4c35525e0e
Francesco Lazzarino <flazzarino@gmail.com>
2009-03-18 00:54:07 -0400

    halorgium's issues

---
+++ spec/feature_requests_spec.rb
@@ -0,0 +1,5 @@
+describe "feature requests" do
+  it "should work with http://www.zvon.org/xxl/XMLSchemaTutorial/Output/ser_keys_st0.html examples"
+  # if current() isnot implemented then look into implementing
+  # extensions to libxslt http://xmlsoft.org/XSLT/extensions.html
+end
