c412e43c477993ca704c2fc3b6ee15e519807e66
Francesco Lazzarino <flazzarino@gmail.com>
2009-03-17 23:46:10 -0400

    factored stuff out, might not work ...

---
+++ lib/schematron.rb
@@ -0,0 +1,53 @@
+require 'libxml'
+require 'libxslt'
+
+module Schematron
+
+  # The location of the ISO schematron implemtation lives
+  ISO_IMPL_DIR = 'iso_impl'
+
+  # The file names of the compilation stages
+  ISO_FILES = [ 'iso_dsdl_include.xsl',
+                'iso_abstract_expand.xsl',
+                'iso_svrl.xsl' ]
+
+  # Namespace prefix declarations for use in XPaths
+  NS_PREFIXES = {
+    'svrl' => 'http://purl.oclc.org/dsdl/svrl'
+  }
+
+  class Schema
+
+    def initialize(doc)
+      @schema_doc = doc
+    end
+
+    def validate(instance_doc)
+
+      # Tell the parse to remember the line numbers for each node
+      # TODO make a new parse for this and not mess with defaults
+      old_line_numbers = XML::Parser::default_line_numbers
+      XML::Parser::default_line_numbers = true
+
+      # Compile schematron into xsl that maps to svrl
+      xforms = ISO_FILES.map do |file|
+        doc = Dir.chdir(ISO_IMPL_DIR) { XML::Document.file file }
+        XSLT::Stylesheet.new doc
+      end
+      
+      validator_doc = xforms.inject(schema_doc) { |xml, xsl| xsl.apply xml }
+      validator_xsl = XSLT::Stylesheet.new validator_doc
+      
+      # Validate the xml
+      results_doc = validator_xsl.apply instance_doc
+      
+      # restore old state
+      XML::Parser::default_line_numbers = old_line_numbers
+      
+      # return the results
+      results_doc
+    end
+
+  end
+
+end
