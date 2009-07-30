ef241b2c3792ea201b7915a1fd553a6e94ea81b2
marly <marlified@gmail.com>
2009-07-30 12:01:31 -0400

    adding support for schematron report rules

--- lib/schematron.rb
+++ lib/schematron.rb
@@ -43,23 +43,31 @@ module Schematron
       # Validate the xml
       results_doc = @validator_xsl.apply instance_doc
 
-      # compile the errors
+      # compile the errors and log any messages
+      rule_hits(results_doc, instance_doc, 'assert', '//svrl:failed-assert') +
+      rule_hits(results_doc, instance_doc, 'report', '//svrl:successful-report')
+    end
+    
+    # Look for reported or failed rules of a particular type in the instance doc
+    def rule_hits(results_doc, instance_doc, rule_type, xpath)
+      
       results = []
       
-      results_doc.root.find('//svrl:failed-assert', NS_PREFIXES).each do |assert|
-        context = instance_doc.root.find_first assert['location']
-
-        assert.find('svrl:text/text()', NS_PREFIXES).each do |message|
+      results_doc.root.find(xpath, NS_PREFIXES).each do |hit|
+        context = instance_doc.root.find_first hit['location']
+        
+        hit.find('svrl:text/text()', NS_PREFIXES).each do |message|
           results << {
+            :rule_type => rule_type,
             :type => context.node_type_name,
             :name => context.name,
             :line => context.line_num,
             :message => message.content.strip }
         end
-
       end
       
       results
+      
     end
 
   end
